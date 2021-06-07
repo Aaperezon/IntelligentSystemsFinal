40db951b6f639991dfeddce97dea1486029542d3
Miguel235711 <a01423629@itesm.mx>
2021-06-07 16:23:57 -0500

    transform to DataBase object and implemented Multinominal Naive Bayes, Decision Tree and Random Forest

--- ReadFile.py
+++ ReadFile.py
@@ -1,5 +1,6 @@
 import string
 import nltk
+import pandas as pd
 nltk.download('stopwords')
 nltk.download('wordnet')
 from nltk.corpus import stopwords
@@ -12,11 +13,18 @@ print("AFTER LEMMATIZER INSTANCE")
 file1 = open("English.txt", 'r')
 Lines = file1.readlines()
 stop_words = stopwords.words("english")
+
+#read class file and join with english.txt
+
+classes_dict = {tp[0]:tp[2] for tp in pd.read_csv('Gold-Ingles.csv',header=None).to_numpy()}
+
+
 def NormalizeRows():
     normalizedLines = []
     for line in Lines:
         line = (''.join([i for i in line if i not in string.punctuation])).lower().split("\t")
         line[2] = ' '.join( [lmtzr.lemmatize(i) for i in line[2].strip().split() if i not in stop_words] )
+        line.append(classes_dict[line[0]])
         normalizedLines.append(line)
 
     return normalizedLines
--- description.py
+++ description.py
@@ -1,8 +1,10 @@
 from ReadFile import NormalizeRows
+from math import log2
+from collections import Counter
+from dataclasses import dataclass
+from random import shuffle
 
-
-
-
+#change to object (id,language,text,tf_idf) to improve readability and optimize
 
 normalizedLines = NormalizeRows()
 print(normalizedLines[0])
@@ -18,18 +20,63 @@ def GetVocabulary(data_base):
             vocabulary.add(word)
     return vocabulary
 
-def VectorizeDataBase(data_base):
+@dataclass
+class DataBaseRow:
+    id: str
+    language: str
+    text: str
+    instance_class: str
+    feature_list: []
+@dataclass
+class DataBase:
+    data_base: []
+    def get_feature_matrix(self):
+        return [row.feature_list for row in self.data_base]
+    def get_class_list(self):
+        return [row.instance_class for row in self.data_base]
+
+def GetDataBaseObjectWithWordVector(data_base):
+    #return array of objects instead of matrix
     vocabulary = GetVocabulary(data_base)
     word_frequency_in_docs = {}
+    document_count = len(data_base)
     for row in data_base:
         for i in set(row[2].split()):
             if i not in word_frequency_in_docs:
                 word_frequency_in_docs[i] = 1
             else:
                 word_frequency_in_docs[i] += 1
-    print(word_frequency_in_docs)
+    word_weight = {k:log2(document_count/v) for k,v in word_frequency_in_docs.items()}
+    data_base_out = []
+    for row in data_base:
+        word_frecuency = Counter(row[2].split())
+        data_base_out.append(DataBaseRow(
+            id=row[0]
+            ,language=row[1]
+            ,text=row[2]
+            ,instance_class=row[3]
+            ,feature_list=[(word_frecuency[word]*weight if word in word_frecuency else 0) for word,weight in word_weight.items()]))
+    shuffle(data_base_out)
+    return DataBase(data_base=data_base_out)
+    #print(word_frequency_in_docs)
 
-                
-VectorizeDataBase(normalizedLines)
+def GetAverageTextLengthAndWordCount(data_base):
+    per_class = {}
+    for row in data_base:
+        class_name = row[3]
+        text_length = len(row[2])
+        word_count = len(row[2].split())
+        if class_name in per_class:
+            per_class[class_name][0]+= text_length
+            per_class[class_name][1]+=1
+            per_class[class_name][2]+=word_count
+        else:
+            per_class[class_name] = [text_length,1,word_count]
+    return {k:(v[0]/v[1],v[2]) for k,v in per_class.items()}
+
+avg_text_length_and_word_count_per_class = GetAverageTextLengthAndWordCount(normalizedLines)
+for k,v in avg_text_length_and_word_count_per_class.items():
+    print(f'class={k} Avg Text Length: {v[0]} Word Count: {v[1]}')
+data_base = GetDataBaseObjectWithWordVector(normalizedLines)
 vocabulary = GetVocabulary(normalizedLines)
 print(f"vocabulary: {vocabulary}")
